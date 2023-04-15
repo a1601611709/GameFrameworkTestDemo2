461130c18b08c44e6005b448914000706b7a98f8
Moyuu01 <a1601611709@126.com>
2023-04-15 23:05:01 +0800

    feat:添加游戏launch

---
+++ Assets/GameMain/Scripts/Procedure/ProcedureLaunch.cs
@@ -0,0 +1,23 @@
+
+using GameFramework.Fsm;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace StarForceDemo
+{
+    public class ProcedureLaunch : ProcedureBase
+    {
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+            Log.Info("Hello world");
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+        }
+
+    }
+}
+
